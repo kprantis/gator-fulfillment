b33a978809a0887a3f0057df3c5712577a740211
Kelsey Prantis <krw1243@gmail.com>
2012-12-04 23:37:41 -0700

    Add models for inventory items.

--- orders/models.py
+++ orders/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
-# Create your models here.
+class InventoryItem(models.Model):
+    """
+    Represents a type of inventory item (eg, washers, bolts, etc)/
+
+    name        A descriptive name for the piece of inventory
+    image_url   A url to an image of the item
+    orderable   True if a item can be directly ordered, False if it is only
+                included as a part with another orderable item.
+    """
+    name = models.CharField(max_length=200)
+    image_url = models.URLField()
+    orderable = models.BooleanField()
+
+class InventoryItemLink(models.Model):
+    """
+    Represents a parent-child relationship between two inventory items.
+
+    Some inventory items require additional parts to be sent along.
+    Eg, a post may require a certain number of bolts and washers to be
+    send along with it for assembly.
+
+    When you order a parent_inventory item, num_child_inventory_items_required
+    will also be added to the order.
+    """
+    parent_inventory_item = models.ForeignKey(InventoryItem, related_name='+')
+    child_inventory_item = models.ForeignKey(InventoryItem, related_name='+')
+    num_child_inventory_items_required = models.IntegerField()
--- orders/tests.py
+++ orders/tests.py
@@ -6,11 +6,37 @@ Replace this with more appropriate tests for your application.
 """
 
 from django.test import TestCase
+from orders.models import InventoryItem, InventoryItemLink
 
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
+class InventoryModelTest(TestCase):
+
+    def test_can_create_inventory_items(self):
+        # Make sure we are starting with the expected empty tables.
+        self.assertFalse(InventoryItem.objects.all())
+        self.assertFalse(InventoryItemLink.objects.all())
+        
+        # Add two inventory items
+        parent_item = InventoryItem.objects.create(
+            name = "Parent", 
+            image_url = '',
+            orderable = True
+        )
+        child_item = InventoryItem.objects.create(
+            name="Child",
+            image_url='',
+            orderable = False
+        )
+        self.assertEqual(2, len(InventoryItem.objects.all()))
+        self.assertTrue(parent_item.orderable)
+        
+        # Link the items into a parent-child relationship where the
+        # parent item requires a certain number of child items.
+        item_link = InventoryItemLink.objects.create(
+            parent_inventory_item = parent_item,
+            child_inventory_item = child_item,
+            num_child_inventory_items_required = 5
+        )
+        self.assertEqual(1, len(InventoryItemLink.objects.all()))
+        self.assertEqual('Parent', item_link.parent_inventory_item.name)
+        self.assertEqual('Child', item_link.child_inventory_item.name)
