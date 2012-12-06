03b07815dbd5dba01f7a18069baee15c82b5ca12
Kelsey Prantis <krw1243@gmail.com>
2012-12-05 18:14:30 -0700

    Use a proper Django many-to-many relationship model.

--- inventory/models.py
+++ inventory/models.py
@@ -6,12 +6,21 @@ class InventoryItem(models.Model):
 
     name        A descriptive name for the piece of inventory
     image_url   A url to an image of the item
+    required_items  A many-to-many relationship - see InventoryItemLink
     orderable   True if a item can be directly ordered, False if it is only
                 included as a part with another orderable item.
     """
     name = models.CharField(max_length=200)
     image_url = models.URLField()
+    required_items = models.ManyToManyField(
+        "self",
+        through = 'InventoryItemLink',
+        symmetrical = False
+    )
     orderable = models.BooleanField()
+
+    def __unicode__(self):
+        return self.name
 
 class InventoryItemLink(models.Model):
     """
@@ -27,3 +36,12 @@ class InventoryItemLink(models.Model):
     parent_inventory_item = models.ForeignKey(InventoryItem, related_name='+')
     child_inventory_item = models.ForeignKey(InventoryItem, related_name='+')
     num_child_inventory_items_required = models.IntegerField()
+
+    def __unicode__(self):
+        return "A %s requires %s %s%s" % (
+            self.parent_inventory_item,
+            self.num_child_inventory_items_required,
+            self.child_inventory_item,
+            's' if self.num_child_inventory_items_required > 1 else ''
+        )
+        
