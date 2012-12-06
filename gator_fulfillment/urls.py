cfc97ade22a9db9d303dcd6c3155fcd20f8d1e12
Kelsey Prantis <krw1243@gmail.com>
2012-12-05 23:35:44 -0700

    Skeleton for printing labels.

--- gator_fulfillment/urls.py
+++ gator_fulfillment/urls.py
@@ -15,6 +15,8 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
+    
+    url(r'^orders/label/(?P<order_id>\d+)/$', 'orders.views.label')
 )
 
 # urls for statis files
--- orders/views.py
+++ orders/views.py
@@ -1,1 +1,4 @@
-# Create your views here.
+from django.http import HttpResponse
+
+def label(request, order_id):
+    return HttpResponse("Create a label for order_id %s" % order_id)
