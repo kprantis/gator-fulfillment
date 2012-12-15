bac8d0ed64d153bf3df5d107f7c39ff3c8d509ae
Kelsey Prantis <krw1243@gmail.com>
2012-12-14 18:54:38 -0700

    Skeleton for loading a template for the label view.

--- orders/views.py
+++ orders/views.py
@@ -1,4 +1,17 @@
 from django.http import HttpResponse
+from django.template import Context, loader
+
+from orders.models import Order
 
 def label(request, order_id):
-    return HttpResponse("Create a label for order_id %s" % order_id)
+    """
+    Create a printable sheet of labels for each of the items associated
+    with a order.
+    """
+    order = Order.objects.get(pk=order_id)
+
+    template = loader.get_template('orders/label.html')
+    context = Context({
+        'order' : order
+    })
+    return HttpResponse(template.render(context))
