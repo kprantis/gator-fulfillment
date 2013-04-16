317369c5193a897aed9c255f0142a7b17f09d374
Kelsey Prantis <krw1243@gmail.com>
2013-04-15 19:04:49 -0600

    Adding SendGrid email settings to settings.py

--- gator_fulfillment/settings.py
+++ gator_fulfillment/settings.py
@@ -161,6 +161,12 @@ LOGGING = {
     }
 }
 
+EMAIL_HOST_USER = os.environ['SENDGRID_USERNAME']
+EMAIL_HOST= 'smtp.sendgrid.net'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_PASSWORD = os.environ['SENDGRID_PASSWORD']
+
 import dj_database_url
 DATABASES['default'] = dj_database_url.config()
 
